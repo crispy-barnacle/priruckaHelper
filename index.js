9169dfe1af1a2b4d366086668e01c8f679cd4cf1
crispy-barnacle <83539938+crispy-barnacle@users.noreply.github.com>
2022-02-15 10:24:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,10 @@
     var tables = document.getElementsByClassName("para");
     for (var tableNr = 0; tableNr < tables.length; tableNr++) {
       var firstColumn = tables[tableNr].getElementsByClassName("vlevo");
-      for (var i = 0; i < firstColumn.length; i++) {
-        firstColumn[i].innerText += " (" + pady[i] + ")";
+      if (firstColumn.length == 7) {
+          for (var i = 0; i < firstColumn.length; i++) {
+            firstColumn[i].innerText += " (" + pady[i] + ")";
+          }
       }
     }
 })();
