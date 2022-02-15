dc0a9a8a0f911b89d1b7c6c722019885f06e75f3
crispy-barnacle <83539938+crispy-barnacle@users.noreply.github.com>
2022-02-15 10:11:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,14 +11,14 @@
 
 (function() {
     'use strict';
+    
+    var pady = ["Nominativ", "Genitiv", "Dativ", "Akuzativ", "Vokativ", "Lok\u00e1l", "Instrument\u00e1l"];
 
-      var pady = ["Nominativ", "Genitiv", "Dativ", "Akuzativ", "Vokativ", "Lokál", "Instrumentál"];
-
-      var tables = document.getElementsByClassName("para");
-      for (var tableNr = 0; tableNr < tables.length; tableNr++) {
-        var firstColumn = tables[tableNr].getElementsByClassName("vlevo");
-        for (var i = 0; i < firstColumn.length; i++) {
-          firstColumn[i].innerText += " (" + pady[i] + ")";
-        }
+    var tables = document.getElementsByClassName("para");
+    for (var tableNr = 0; tableNr < tables.length; tableNr++) {
+      var firstColumn = tables[tableNr].getElementsByClassName("vlevo");
+      for (var i = 0; i < firstColumn.length; i++) {
+        firstColumn[i].innerText += " (" + pady[i] + ")";
       }
+    }
 })();
