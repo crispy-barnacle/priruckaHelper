f0270fadb67aabe14516028064aeaa141c9eea1e
crispy-barnacle <83539938+crispy-barnacle@users.noreply.github.com>
2022-02-15 13:27:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         priruckaHelper
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  try to take over the world!
 // @author       Me
 // @match        https://prirucka.ujc.cas.cz/*
@@ -14,7 +14,7 @@
     
     var pady = ["Nominativ", "Genitiv", "Dativ", "Akuzativ", "Vokativ", "Lok\u00e1l", "Instrument\u00e1l"];
     var padyRus = ["Именительный", "Родительный", "Дательный", "Винительный", "Звательный", "Предложный", "Творительный"];
-    var questions = ["kdo? со?", "koho? \u010deho?", "komu? \u010demu?", "koho? co?", "-", "(o) kom? (o) \u010dem?", "kým? \u010d\u00edm"];
+    var questions = ["kdo? co?", "koho? \u010deho?", "komu? \u010demu?", "koho? co?", "-", "(o) kom? (o) \u010dem?", "k\u00fdm? \u010d\u00edm"];
 
     var tables = document.getElementsByClassName("para");
     for (var tableNr = 0; tableNr < tables.length; tableNr++) {
