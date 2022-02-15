26a5c47d95d49176bd6754a92cf5c3db893978cd
crispy-barnacle <83539938+crispy-barnacle@users.noreply.github.com>
2022-02-15 13:31:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         priruckaHelper
 // @namespace    http://tampermonkey.net/
-// @version      0.7
+// @version      0.8
 // @description  try to take over the world!
 // @author       Me
 // @match        https://prirucka.ujc.cas.cz/*
@@ -14,6 +14,13 @@
     
     var pady = ["Nominativ", "Genitiv", "Dativ", "Akuzativ", "Vokativ", "Lok\u00e1l", "Instrument\u00e1l"];
     var padyRus = ["Именительный", "Родительный", "Дательный", "Винительный", "Звательный", "Предложный", "Творительный"];
+    padyRus = ["\u0418\u043C\u0435\u043D\u0438\u0442\u0435\u043B\u044C\u043D\u044B\u0439\u000A", 
+               "\u0420\u043E\u0434\u0438\u0442\u0435\u043B\u044C\u043D\u044B\u0439",
+               "\u0414\u0430\u0442\u0435\u043B\u044C\u043D\u044B\u0439",
+               "\u0412\u0438\u043D\u0438\u0442\u0435\u043B\u044C\u043D\u044B\u0439",
+               "\u0417\u0432\u0430\u0442\u0435\u043B\u044C\u043D\u044B\u0439",
+               "\u041F\u0440\u0435\u0434\u043B\u043E\u0436\u043D\u044B\u0439",
+               "\u0422\u0432\u043E\u0440\u0438\u0442\u0435\u043B\u044C\u043D\u044B\u0439"]
     var questions = ["kdo? co?", "koho? \u010deho?", "komu? \u010demu?", "koho? co?", "-", "(o) kom? (o) \u010dem?", "k\u00fdm? \u010d\u00edm"];
 
     var tables = document.getElementsByClassName("para");
